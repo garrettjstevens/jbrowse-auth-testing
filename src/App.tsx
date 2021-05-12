bfe38c1ae49994e47e227ea027a376f50019c2a8
Garrett Stevens <stevens.garrett.j@gmail.com>
2021-05-12 10:39:34 -0600

    Simple app working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BigWig } from '@gmod/bbi'
+import { RemoteFile } from 'generic-filehandle'
 import './App.css';
 
 function App() {
+  const [bigWigUrl, setBigWigUrl] = useState('')
+  const [bigWigHeader, setBigWigHeader] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  function onBigWigUrlChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setErrorMessage('')
+    setBigWigUrl(event.target.value)
+  }
+
+  async function onFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    setErrorMessage('')
+    let bigWigUrlValidated: string
+    try {
+      const url = new URL(bigWigUrl)
+      bigWigUrlValidated = url.href
+    } catch (error) {
+      console.error(error)
+      setErrorMessage('invalid URL')
+      return
+    }
+    const filehandle = new RemoteFile(bigWigUrlValidated)
+    const file = new BigWig({ filehandle })
+    const header = await file.getHeader()
+    setBigWigHeader(JSON.stringify(header, null, 2))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={onFormSubmit}>
+      <label>
+        BigWig URL:
+        <input
+          size={75}
+          type="text"
+          name="name"
+          value={bigWigUrl}
+          onChange={onBigWigUrlChange}
+          required
+        />
+      </label>
+      <br />
+      <input type="submit" value="Get header" />
+    </form>
+    <div style={{color: 'red'}}>{errorMessage}</div>
+    <textarea readOnly value={bigWigHeader} rows={70} cols={50}/>
     </div>
   );
 }
