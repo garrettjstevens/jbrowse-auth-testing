bb42f19585a11dddb1615a3c2dbf3944703934ea
peterkxie <peterkxie@yahoo.com>
2021-05-24 16:45:44 -0700

    fetch a file from google drive and obtain the title, add some error handling

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,19 @@ function App() {
   const [bigWigUrl, setBigWigUrl] = useState('')
   const [bigWigHeader, setBigWigHeader] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [tokenInfo, setTokenInfo] = useState({
+    token_type: '',
+    access_token: '',
+  })
+  const [files, setFiles] = useState([])
 
   function onBigWigUrlChange(event: React.ChangeEvent<HTMLInputElement>) {
     setErrorMessage('')
     setBigWigUrl(event.target.value)
   }
+
+  function getIdFromUrl(url: string) { return url.match(/[-\w]{25,}/); }
 
   async function onFormSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -26,17 +34,45 @@ function App() {
       setErrorMessage('invalid URL')
       return
     }
-    const filehandle = new RemoteFile(bigWigUrlValidated)
-    const file = new BigWig({ filehandle })
-    const header = await file.getHeader()
-    setBigWigHeader(JSON.stringify(header, null, 2))
+
+    if(bigWigUrlValidated.includes('google.com')){
+      if(!loggedIn){
+        setErrorMessage('Not Authorized')
+        return
+      }
+      const urlId = getIdFromUrl(bigWigUrlValidated)
+      console.log(urlId ? urlId[0] : '')
+      try{
+        const response = await fetch(`https://www.googleapis.com/drive/v2/files/${urlId}`, {
+          credentials: 'include',
+          headers: {
+            'Authorization': `${tokenInfo.token_type} ${tokenInfo.access_token}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          }
+        })
+        const json = await response.json()
+        setBigWigHeader(json.title)
+        console.log('file', json)
+      }
+      catch(error){
+        console.error(error)
+        setErrorMessage('could not fetch file')
+        return
+      }
+    }
+    else{
+      const filehandle = new RemoteFile(bigWigUrlValidated)
+      const file = new BigWig({ filehandle })
+      const header = await file.getHeader()
+      setBigWigHeader(JSON.stringify(header, null, 2))
+    }
   }
 
-
-  const responseGoogle = async (response: any) => {
-    console.log(response);
-    const tokenInfo = response.tokenObj
-    console.log(tokenInfo.access_token)
+  async function fetchAllFiles() {
+    if(!loggedIn){ 
+      setErrorMessage('Not Authorized')
+      return
+    }
     let fetchResponse
     try {
       fetchResponse = await fetch('https://www.googleapis.com/drive/v3/files', {
@@ -53,6 +89,16 @@ function App() {
 
     const json = await fetchResponse?.json()
     console.log(json)
+    setFiles(json)
+  }
+
+
+  const responseGoogle = async (response: any) => {
+    console.log(response);
+    setLoggedIn(true)
+    const tokenInfo = response.tokenObj
+    console.log(tokenInfo.access_token)
+    setTokenInfo(tokenInfo)
   }
 
   return (
@@ -72,12 +118,13 @@ function App() {
       <br />
       <input type="submit" value="Get header" />
     </form>
+    <button onClick={fetchAllFiles}> Fetch all files</button>
     <div style={{color: 'red'}}>{errorMessage}</div>
     <textarea readOnly value={bigWigHeader} rows={70} cols={50}/>
     <GoogleLogin 
       clientId='20156747540-bes2tq75790efrskmb5pa3hupujgenb2.apps.googleusercontent.com'
       buttonText='Login'
-      scope='https://www.googleapis.com/auth/drive'
+      scope='https://www.googleapis.com/auth/drive.metadata'
       // scope='https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.metadata.readonly	https://www.googleapis.com/auth/drive.readonly'
       cookiePolicy={'single_host_origin'}
       onSuccess={responseGoogle}
