5e04871d42f676fc533afc9662f255c86bbc4c2d
peterkxie <peterkxie@yahoo.com>
2021-06-01 17:41:27 -0700

    get access_token from dropbox, start on obtaining file contents (can download)

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,68 @@ function App() {
     token_type: '',
     access_token: '',
   })
+  const [dbTokenInfo, setDbTokenInfo] = useState({
+    token_type: '',
+    access_token: '',
+  })
+  const [dropboxCode, setDropboxCode] = useState('')
+  
+  // parse location, if there is a code param then send info to parent and close the window
+  // and do a window.opener.postMessage, and probably add an event listener to receieve the postMessage
   const [files, setFiles] = useState<any[]>([])
-  const [windowHandle, setWindowHandle] = useState<Window | null>(null)
 
+  if(window && window.location.href.includes('code'))
+  {
+    console.log('inside')
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    const code = urlParams.get('code')
+    const parent = window.opener
+    if(code && parent){
+      parent.postMessage({code: code}, 'http://localhost:3000')
+      setLoggedIn(true)
+      window.close()
+    }
+  }
+
+  window.addEventListener("message", event => {
+    if(event.data.code){
+      setDropboxCode(event.data.code)
+    }
+  })
+
+  useEffect(() => {
+    async function getDropboxToken(){
+      const data = {
+        code: dropboxCode,
+        grant_type: 'authorization_code',
+        client_id: 'wyngfdvw0ntnj5b',
+        client_secret: 'o1qarh66eu1m48y',
+        redirect_uri:'http://localhost:3000'
+      }
+
+      const params = Object.entries(data)
+      .map(([key, val]) => `${key}=${encodeURIComponent(val)}`)
+      .join('&')
+
+      if(dropboxCode){
+        const response = await fetch('https://api.dropbox.com/oauth2/token', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: params,
+        })
+        const token = await response.json()
+        console.log(token)
+        setDbTokenInfo({
+          token_type: token.token_type,
+          access_token: token.access_token,
+        })
+      }
+    }
+    getDropboxToken()
+  }, [dropboxCode])
   function onBigWigUrlChange(event: React.ChangeEvent<HTMLInputElement>) {
     setErrorMessage('')
     setBigWigUrl(event.target.value)
@@ -46,10 +105,9 @@ function App() {
         const response = await fetchGoogleDriveFile(urlId)
         if(response){
           const test = await response.json()
-          console.log(test)
           const filehandle = new RemoteFile(test.downloadUrl, {
             overrides: {
-              credentials: 'include',
+              // credentials: 'include',
               headers: {
                 'Authorization': `${tokenInfo.token_type} ${tokenInfo.access_token}`,
                 'Content-Type': 'application/octet-stream'
@@ -62,11 +120,65 @@ function App() {
           setBigWigHeader(JSON.stringify(header, null, 2))
         }
     }
+    else if(bigWigUrlValidated.includes('dropbox.com')){
+      // if(!loggedIn){
+      //   setErrorMessage('Not authorized')
+      //   return
+      // }
+      const response = await fetch('https://api.dropboxapi.com/2/sharing/get_shared_link_metadata',{
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${dbTokenInfo.access_token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          url: bigWigUrlValidated
+        }),
+      })
+      const json = await response.json()
+      console.log(json)
+      if(json.id){
+        // const data = {
+        //   id: json.id.replace('id:', '')
+        // }
+        // const fileResponse = await fetch('https://api.dropboxapi.com/2/file_requests/get', {
+        //   method: 'POST',
+        //   headers: {
+        //     'Authorization': `Bearer ${dbTokenInfo.access_token}`,
+        //     'Content-Type': 'application/json'
+        //   },
+        //   body: JSON.stringify(data)
+        // })
+        // const data = {
+        //   path: json.id
+        // }
+        // const fileResponse = await fetch('https://content.dropboxapi.com/2/files/download', {
+        //   method: 'POST',
+        //   headers: {
+        //     'Authorization': `Bearer ${dbTokenInfo.access_token}`,
+        //     'Content-Type': 'application/octet-stream',
+        //     'Dropbox-API-Arg': JSON.stringify(data),
+        //   },
+        // })
+        const filehandle = new RemoteFile(bigWigUrlValidated, {
+          overrides: {
+            credentials: 'include',
+            headers: {
+              'Authorization': `${dbTokenInfo.token_type} ${dbTokenInfo.access_token}`,
+              'Content-Type': 'application/octet-stream'
+            }
+          }
+        })
+        const file = new BigWig({ filehandle })
+        const header = await file.getHeader()
+        console.log(test)
+        setBigWigHeader(JSON.stringify(header, null, 2))
+      }
+    }
     else{
       const filehandle = new RemoteFile(bigWigUrlValidated)
       const file = new BigWig({ filehandle })
       const header = await file.getHeader()
-      console.log(filehandle, file, header)
       setBigWigHeader(JSON.stringify(header, null, 2))
     }
   }
@@ -108,16 +220,13 @@ function App() {
     }
 
     const json = await fetchResponse?.json()
-    console.log(json)
     setFiles(json.files)
   }
 
 
   const responseGoogle = async (response: any) => {
-    console.log(response);
     setLoggedIn(true)
     const tokenInfo = response.tokenObj
-    console.log(tokenInfo.access_token)
     setTokenInfo(tokenInfo)
   }
 
@@ -132,24 +241,8 @@ function App() {
     .map(([key, val]) => `${key}=${encodeURIComponent(val)}`)
     .join('&')
 
-    setWindowHandle(createOauthWindow(`https://www.dropbox.com/oauth2/authorize?${params}`))
-  //   if(windowHandle)
-  //   {  
-  //     console.log(windowHandle)
-  //     windowHandle.addEventListener("message", (event) => {
-  //       console.log(event)
-  //     })
-  //  }
+    createOauthWindow(`https://www.dropbox.com/oauth2/authorize?${params}`)
   }
-
-  useEffect(() => {
-    console.log('here')
-    console.log(windowHandle)
-
-    // get href, close if there is a code, send code to parent(opener)
-
-  }, [windowHandle])
-
 
   function createOauthWindow(url: string, name = 'Authorization', width = 500, height = 600, left = 0, top = 0) {
     const options =   `width=${width},height=${height},left=${left},top=${top}`;
@@ -190,10 +283,8 @@ function App() {
                 <tr key={file.name} >
                   <td onClick={async() =>{ 
                     const response = await fetchGoogleDriveFile(file.id)
-                    console.log('here')
                     if(response){
                       const test = await response.json()
-                      console.log(test)
                       const filehandle = new RemoteFile(test.downloadUrl, {
                         overrides: {
                           headers: {
@@ -204,7 +295,6 @@ function App() {
                       })
                       const file = new BigWig({ filehandle })
                       const header = await file.getHeader()
-                      console.log(test)
                       setBigWigHeader(JSON.stringify(header, null, 2))
                     }
                   }}>{file.name}</td>
